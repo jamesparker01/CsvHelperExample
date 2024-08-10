885330c1994db6a067aca768c66cc7ebafe64d5e
James Parker <james.parker01@gmail.com>
2024-08-10 07:52:09 +0100

    Add example and data

---
+++ CsvHelperExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+
+var csv = new CsvRepository().ReadCsv().ToArray();
+
+foreach (var row in csv)
+{
+    Console.WriteLine($"Id: {row.Id} Name: {row.Name}");
+}
+Console.ReadKey();
+
+public class CsvRepository
+{
+    public IEnumerable<Foo> ReadCsv()
+    {
+        using (var reader = new StreamReader("testdata.csv"))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            csv.Context.RegisterClassMap<FooMap>();
+            return csv.GetRecords<Foo>().ToList();
+        }
+    }
+}
+
+public record Foo
+{
+    public string Id { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+}
+
+public sealed class FooMap : ClassMap<Foo>
+{
+    public FooMap()
+    {
+        Map(m => m.Id).Name("ColumnA");
+        Map(m => m.Name).Name("ColumnD");
+    }
+}
